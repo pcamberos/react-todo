c2226d2acadc0da1bacbbbade79186ba1a95713b
juan.camberos@estrasol.com.mx <juan.camberos@estrasol.com.mx>
2019-05-15 21:44:33 -0500

    Se modifico el componente principal par agregar las secciones del Todo!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Checkbox from '@material-ui/core/Checkbox';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 function App() {
   return (
     //Esto es jsx
-    <div>
-      <Typography variant="h1" align="center" gutterBottom> 
-        Todos!
-      </Typography>
+    <React.Fragment>
+        <Typography variant="h2" align="center" gutterBottom> ¡Todos! </Typography>
 
-      <TextField type="text" placeholder="Write Here!" margin="normal" variant="outlined">
+        <div align="center">
+          <TextField type="text" placeholder="Write Here!" margin="normal" variant="outlined"></TextField>  
+        </div>
 
-      </TextField>  
-    </div>
-  
+        <List>
+        {[0, 1, 2, 3].map(value => (
+            <ListItem dense button>
+            <Checkbox></Checkbox>
+            <ListItemText primary={`Opción ${value + 1}`} ></ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton>
+                <DeleteIcon/>
+              </IconButton>
+            </ListItemSecondaryAction>
+            <DeleteIcon></DeleteIcon>
+        </ListItem>
+        ))}
+        </List>
+  </React.Fragment>
   );
 }
 
